e98ede836cad9d07b1b797235c468ba5768d9682
Manuel Cuevas <manuelcuevasrod96@gmail.com>
2018-01-31 13:04:26 +0100

    Inicio de videoguay

---
+++ models/Peliculas.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "peliculas".
+ *
+ * @property int $id
+ * @property string $codigo
+ * @property string $titulo
+ * @property string $precio_alq
+ *
+ * @property Alquileres[] $alquileres
+ */
+class Peliculas extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'peliculas';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['codigo', 'titulo', 'precio_alq'], 'required'],
+            [['codigo', 'precio_alq'], 'number'],
+            [['titulo'], 'string', 'max' => 255],
+            [['codigo'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'codigo' => 'Codigo',
+            'titulo' => 'Titulo',
+            'precio_alq' => 'Precio Alq',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAlquileres()
+    {
+        return $this->hasMany(Alquileres::className(), ['pelicula_id' => 'id'])->inverseOf('pelicula');
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSocios()
+    {
+        return $this->hasMany(Socios::className(), ['id' => 'socio_id'])
+        ->via('alquileres');
+    }
+}
---
+++ models/Socios.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "socios".
+ *
+ * @property int $id
+ * @property string $numero
+ * @property string $nombre
+ * @property string $direccion
+ * @property string $telefono
+ *
+ * @property Alquileres[] $alquileres
+ */
+class Socios extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'socios';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['numero', 'nombre'], 'required'],
+            [['numero', 'telefono'], 'number'],
+            [['nombre', 'direccion'], 'string', 'max' => 255],
+            [['numero'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'numero' => 'Numero',
+            'nombre' => 'Nombre',
+            'direccion' => 'Direccion',
+            'telefono' => 'Telefono',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAlquileres()
+    {
+        return $this->hasMany(Alquileres::className(), ['socio_id' => 'id'])->inverseOf('socio');
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPeliculas()
+    {
+        return $this->hasMany(Peliculas::className(), ['id' => 'pelicula_id'])
+        ->via('alquileres');
+    }
+}
